6b840cc637a5397b999e7823bde521d650c96676
Vlad Apostol <vladapostol1029@gmail.com>
2024-04-23 21:15:04 +0300

    Init JwtToken

--- backend/QuickServe/Program.cs
+++ backend/QuickServe/Program.cs
@@ -1,14 +1,37 @@
 using QuickServe.Interfaces;
 using QuickServe.Services;
-using QuickServe.Models;
 using QuickServe.Contracts;
+using QuickServe.Utils;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 builder.Services.AddScoped<IDatabaseService, DatabaseService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddSingleton<JwtGenerator>(sp => new JwtGenerator(configuration));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
 
 var app = builder.Build();
+
+
+app.UseAuthentication();
 
 app.MapGet("/", () => "Hello World!");
 
@@ -22,8 +45,13 @@ app.MapPost("/register", async (IAuthService authService, RegistrationContract c
 
 app.MapPost("/login", async (IAuthService authService, LoginContract contract) => 
 {
-    UserModel user = await authService.AuthenticateUser(contract.Username, contract.Password);
-    return user != null ? Results.Ok(user) : Results.Unauthorized();
+    var user = await authService.AuthenticateUser(contract.Username, contract.Password);
+    if (user != null)
+    {
+        //var token = JwtGenerator.GenerateJwtToken(user);
+        return Results.Ok(new { Token = token });
+    }
+    return Results.Unauthorized();
 });
 
 
---
+++ backend/QuickServe/Utils/JwtGenerator.cs
@@ -0,0 +1,43 @@
+﻿namespace QuickServe.Utils;
+
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using QuickServe.Models;
+
+public class JwtGenerator
+{
+    private readonly IConfiguration _configuration;
+
+    public JwtGenerator(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string GenerateJwtToken(UserModel user)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim("restaurant_id", user.RestaurantId.ToString())
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddHours(3),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
