7b2ed5fffb03526819299380e7e707df11f98c5b
Vlad Apostol <vladapostol1029@gmail.com>
2024-05-26 22:01:51 +0300

    Help added

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -4,9 +4,14 @@ import Menu from "../components/Menu";
 import Orders from "../components/Orders";
 import MakeOrder from "../components/MakeOrder";
 import NewFood from "../components/NewFood";
+import Help from "../components/Help";
 
 const Dashboard: React.FC = () => {
   const [appState, setAppState] = useState(0);
+
+  const goToHelp = () => {
+    setAppState(4)
+  }
 
   return (
     <div className="flex flex-row min-h-screen bg-zinc-950">
@@ -20,10 +25,20 @@ const Dashboard: React.FC = () => {
           <MakeOrder />
         ) : appState == 3 ? (
           <NewFood />
+        ) : appState == 4 ? (
+          <Help />
         ) : (
           <div>An error has appeard.</div>
         )}
       </div>
+      {appState !== 4 && (
+  <button 
+    className="absolute bottom-4 right-4 rounded-full px-4 py-2 bg-blue-600 hover:bg-blue-700 border-2 border-white" 
+    onClick={goToHelp}
+  >
+    ?
+  </button>
+)}
     </div>
   );
 };
